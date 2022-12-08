51dda15da1bb5bdb9047912dee2372ac5e875e1b
VladaPlatonovska <vplatonovska@kse.org.ua>
2022-12-08 11:19:13 +0200

    check country

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-with open("Olympic Athletes - athlete_events.tsv") as file:
-    headline = file.readline().split("/t")
+import sys
+
+print(len(sys.argv[1:]))
+
+country = sys.argv[3]
+
+if len(sys.argv) > 6:
+    print("error")
+    exit()
+
+
+# with open("Olympic Athletes - athlete_events.tsv") as file:
+#     headline = file.readline().split("/t")
 
