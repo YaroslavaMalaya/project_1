99dae1fbcda75e81b8afab809018b938b8daad58
VladaPlatonovska <vplatonovska@kse.org.ua>
2022-12-09 20:01:01 +0200

    Яся лови перше завдання

--- main.py
+++ main.py
@@ -3,20 +3,35 @@ import sys
 file1 = sys.argv[0]
 
 
-def medals(country, game):
+def check(new_line, counter):
+    if country in new_line[7]:
+        find_medals(country, game)
+    elif country not in new_line[7]:
+        print("This country didn't take part in games.")
+        exit()
+    elif game not in new_line[8]:
+        print("Invalid year.")
+        exit()
+    elif counter < 10:
+        print("This country has less than 10 medals((((")
+        exit()
+
+
+
+def find_medals(country, game):
     names = []
     medals = []
     counter = 0
     with open("olympics.tsv", "r") as file:
         file.readline()                         # headline
         line = file.readline()
+        print("First ten medalists.")
         while line != "":
             new_line = line.split("\t")
             if country == new_line[7] and game in new_line[8]:
                 while counter < 10:
                     if new_line[1] not in names and new_line[-1] != "NA\n":
                         m = new_line[-1].split("\n")[0]
-                        print("First ten medalists.")
                         print(f"{counter+1}) {new_line[1]} - {new_line[12]} - {m}")
                         counter += 1
                         names.append(new_line[1])
@@ -24,7 +39,26 @@ def medals(country, game):
                         break
                 medals.append(new_line[-1])
             line = file.readline()
+    # check(new_line, counter)
+    return medals
 
+
+def add_in_file():
+    pass
+
+def count_medals():
+    medals = find_medals(country, game)
+    gold = 0
+    silver = 0
+    bronze = 0
+    for medal in medals:
+        if "Gold\n" in medal:
+            gold += 1
+        if "Silver\n" in medal:
+            silver += 1
+        if "Bronze\n" in medal:
+            bronze += 1
+    print(f"amount of gold medals {gold}\namount of silver medals {silver}\namount of bronze medals {bronze} ")
 
 def total():
     pass
@@ -37,5 +71,7 @@ mode = input("Enter a command: ")
 if mode == "-total":
     total()
 elif mode == "-medal":
-    medals(country, game)
+    count_medals()
+elif mode == "-output":
+    find_medals(country, game)
 
