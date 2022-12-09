17864358901efd5ced5c2007e35d340f74f5dd2b
Yaroslava Mala <ymala@kse.org.ua>
2022-12-09 12:31:09 +0200

    First part first task

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 import sys
 
-file1 = sys.argv[1]
-country = input("Enter a country: ")
+file1 = sys.argv[0]
 
-with open("olympics.tsv", "r") as file:
-    line = file.readline()
-    while line != "":
-        split = line.split("\t")
-        if country == split[7]:
-            print(split)
+
+def medals(country, game):
+    names = []
+    medals = []
+    counter = 0
+    with open("olympics.tsv", "r") as file:
+        file.readline()                         # headline
         line = file.readline()
+        while line != "":
+            new_line = line.split("\t")
+            if country == new_line[7] and game in new_line[8]:
+                while counter < 10:
+                    if new_line[1] not in names and new_line[-1] != "NA\n":
+                        m = new_line[-1].split("\n")[0]
+                        print("First ten medalists.")
+                        print(f"{counter+1}) {new_line[1]} - {new_line[12]} - {m}")
+                        counter += 1
+                        names.append(new_line[1])
+                    else:
+                        break
+                medals.append(new_line[-1])
+            line = file.readline()
 
 
+def total():
+    pass
 
 
+country = input("Enter a country: ")
+game = input("Enter a game: ")
+mode = input("Enter a command: ")
 
+if mode == "-total":
+    total()
+elif mode == "-medal":
+    medals(country, game)
 
-# country = sys.argv[3]
-# values = [x for x in sys.argv[2:]]
-# print(values)
